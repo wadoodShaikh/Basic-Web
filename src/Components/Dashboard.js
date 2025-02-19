4b8e29351d50beb7f883ab94005efcc049d6a236
Wadood Shaikh <wadoodshaikh610@example.com>
2025-02-19 17:23:57 +0530

    add Components

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,9 +1,28 @@
+import { GrGallery } from "react-icons/gr";
+import { IoMdDocument } from "react-icons/io";
+
 const Dashboard = () => {
-    return ( 
-        <>
-        <h1>Dashboard</h1>
-        </>
-     );
-}
- 
+  return (
+    <>
+      <h1>Dashboard</h1>
+      <div className="cardsReport">
+        <div className="displayCard">
+          <GrGallery className="cardSvg svgIcon-large" />
+          <div className="cardInfo">
+            <p>Gallery</p>
+            <h2>4</h2>
+          </div>
+        </div>
+        <div className="displayCard">
+          <IoMdDocument className="cardSvg svgIcon-large" />
+          <div className="cardInfo">
+            <p>Document</p>
+            <h2>4</h2>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
 export default Dashboard;
