e64752e4ef480c1f5cce21551257e8a3033e46bf
Wadood Shaikh <wadoodshaikh610@example.com>
2025-02-19 17:25:10 +0530

    add Pages

--- src/Pages/Document.js
+++ src/Pages/Document.js
@@ -1,10 +1,89 @@
 const Document = () => {
-    return ( 
-        <div>
-            <h1>My Document</h1>
-            <p>This is a sample document.</p>
+  return (
+    <>
+      <div className="title">
+        <h1>Documents</h1>
+      </div>
+      <div className="documentTable container">
+        <div className="flexy">
+          <h2>All Documents</h2>
+          <input type="file" id="addFile" className="btn-inp" />
+          <label for="addFile">Add Document</label>
         </div>
-     );
-}
- 
+        <table className="tableProp">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Title</th>
+              <th className="td-min">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="td-min-min">01</td>
+              <td>Doc1</td>
+              <td className="td-min">
+                <button
+                  className="btn btn-danger"
+                  onClick={() => alert("Delete")}
+                >
+                  Delete
+                </button>
+                <button className="btn" onClick={() => alert("Edit")}>
+                  Edit
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td className="td-min-min">02</td>
+              <td>Doc2</td>
+              <td className="td-min">
+                <button
+                  className="btn btn-danger"
+                  onClick={() => alert("Delete")}
+                >
+                  Delete
+                </button>
+                <button className="btn" onClick={() => alert("Edit")}>
+                  Edit
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td className="td-min-min">03</td>
+              <td>Doc3</td>
+              <td className="td-min">
+                <button
+                  className="btn btn-danger"
+                  onClick={() => alert("Delete")}
+                >
+                  Delete
+                </button>
+                <button className="btn" onClick={() => alert("Edit")}>
+                  Edit
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td className="td-min-min">04</td>
+              <td>Doc4</td>
+              <td className="td-min">
+                <button
+                  className="btn btn-danger"
+                  onClick={() => alert("Delete")}
+                >
+                  Delete
+                </button>
+                <button className="btn" onClick={() => alert("Edit")}>
+                  Edit
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
+
 export default Document;
--- src/Pages/Gallery.js
+++ src/Pages/Gallery.js
@@ -1,9 +1,24 @@
-const Gallery = () =>{
-    return (
-        <div>
-            <h1>Gallery</h1>
+import { IoMdAddCircle, IoMdAdd } from "react-icons/io";
+import image from "../Components/image.jpeg";
+
+const Gallery = () => {
+  return (
+    <>
+      <h1>Gallery</h1>
+      <div className="container">
+        <div className="gallery">
+          <input type="file" id="file" className="inp" />
+          <label for="file">
+            <IoMdAdd className="svgIcon-large" />
+          </label>
+          <img src={image} alt="Placeholder" className="img-mid" />
+          <img src={image} alt="Placeholder" className="img-mid" />
+          <img src={image} alt="Placeholder" className="img-mid" />
+          <img src={image} alt="Placeholder" className="img-mid" />
         </div>
-    );
-}
+      </div>
+    </>
+  );
+};
 
 export default Gallery;
